da6380f12446b9fb2d5dcd4b389dc8f1c307bb16
Jamdade Sakshi Ramchandra <158235195+Sakshi-Jamdade@users.noreply.github.com>
2024-12-14 08:40:01 +0530

    Add files via upload

---
+++ Day52/PalindromeChecker.java
@@ -0,0 +1,41 @@
+class PalindromeChecker 
+{
+    public boolean isPalindrome(String s) 
+	{
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) 
+		{
+            if (!Character.isLetterOrDigit(s.charAt(left))) 
+			{
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(right))) 
+			{
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) 
+			{
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) 
+	{
+        PalindromeChecker checker = new PalindromeChecker();
+        String input = "A man, a plan, a canal: Panama";
+        System.out.println("Is the input string a palindrome? " + checker.isPalindrome(input)); // Output: true
+
+        input = "race a car";
+        System.out.println("Is the input string a palindrome? " + checker.isPalindrome(input)); // Output: false
+    }
+}
