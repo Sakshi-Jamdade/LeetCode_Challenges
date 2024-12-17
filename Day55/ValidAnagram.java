5fb5826388d5d46ad509c43f68e351fc8e893684
Jamdade Sakshi Ramchandra <158235195+Sakshi-Jamdade@users.noreply.github.com>
2024-12-17 21:34:40 +0530

    Add files via upload

---
+++ Day55/ValidAnagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ValidAnagram 
+{
+    public static boolean isAnagram(String s, String t) 
+	{
+        if (s.length() != t.length()) 
+		{
+            return false;
+        }
+        
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        
+        return Arrays.equals(sArr, tArr);
+    }
+    
+    public static void main(String[] args) 
+	{
+        System.out.println(isAnagram("anagram", "nagaram"));  // Output: true
+        System.out.println(isAnagram("rat", "car"));  // Output: false
+    }
+}
